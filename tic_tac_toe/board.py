79d0f91b49ecf73f8bce37db956f2ac83a43245a
David Bradburn <david.bradburn2@gmail.com>
2020-03-06 22:00:34 +0000

    Update board.py

--- tic_tac_toe/board.py
+++ tic_tac_toe/board.py
@@ -7,6 +7,8 @@ class board:
         self.width = 3
         self.array = np.zeros((self.height, self.width) , int)
         self.icon = ["    ", "  X ", "  O "]
+        self.state = 1
+        self.win_state = 0
 
     def display_terminal(self):
         for x in range(self.height):
@@ -17,9 +19,105 @@ class board:
             print("\n", end='')
         print("-------------------")
 
+    def check_win(self ,state):
+
+        for x in range(self.width):
+            count = 0
+            for y in range(self.height):
+                if self.array[x][y] == state:
+                    count += 1
+
+            if count == 3:
+                print("Congratulations player {}! You have won!".format(self.icon[state]))
+                self.win_state = 1
+                return
+
+        for y in range(self.height):
+            count = 0
+            for x in range(self.width):
+                if self.array[x][y] == state:
+                    count += 1
+
+            if count == 3:
+                print("Congratulations player {}! You have won!".format(self.icon[state]))
+                self.win_state = 1
+                return
+
+
+        count = 0
+        for x in range(self.width):
+            if self.array[x][x] == state:
+                count += 1
+
+        if count == 3:
+            print("Congratulations player {}! You have won!".format(self.icon[state]))
+            self.win_state = 1
+            return
+
+
+        count = 0
+        for x in range(self.width):
+            if self.array[x][self.width - 1 - x] == state:
+                count += 1
+
+        if count == 3:
+            print("Congratulations player {}! You have won!".format(self.icon[state]))
+            self.win_state = 1
+            return
+        return
+
+    def place(self, x, y, symbol):
+
+        if type(x) != int or type(y) != int:
+            print("Please enter an integer")
+            return
+
+        elif x > self.width or y > self.height:
+            print("Please enter a number in range")
+            return
+
+        if int(self.array[x - 1][y - 1]) != 0:
+            print("There is already a {} there you cannot place a {} on this spot \n Please choose a different spot\n".format(self.icon[self.array[x - 1][y - 1]], self.icon[symbol]))
+            return
+
+        else:
+            self.array[x - 1][y - 1] = symbol
+
+            board.check_win(self, symbol)
+            if self.win_state != 1:
+                if self.state == 1:
+                    self.state = 2
+                else:
+                    self.state = 1
+            return
+
+    def input_icon(self):
+        try:
+            x = int(input("Please select and x between 1 and {} inclusive". format(self.width)))
+        except:
+            print("Please enter an integer")
+            return
+
+        try:
+            y = int(input("Please select and y between 1 and {} inclusive". format(self.height)))
+        except:
+            print("Please enter an integer")
+            return
+
+        board.place(self, x, y, self.state)
+        return
+
+    def main(self):
+
+        board.display_terminal(self)
+
+        while self.win_state != 1 :
+
+            board.input_icon(self)
+            board.display_terminal(self)
 
 
 print("Hello World!!")
 
-test = board()
-test.display_terminal()
+game = board()
+game.main()
